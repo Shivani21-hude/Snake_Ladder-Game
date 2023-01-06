7c5a40ff48def46eb5000eaec84b820d0ee8f10d
Shivani21-hude <shivani21hude@gmail.com>
2023-01-06 19:38:06 +0530

    [Shivani]-Player plays up to winning position

--- SnakeLadder_Game/SnakeLadderGame.cs
+++ SnakeLadder_Game/SnakeLadderGame.cs
@@ -8,39 +8,62 @@ namespace SnakeLadder_Game
 {
     public class SnakeLadderGame
     {
-        public static void UC3SnakeLadder()
+        public static void UC4WinningPosition()
         {
-            int start = 0;
+
             int position = 0;
-            Console.WriteLine("Player at position {0} for to start the game  ", start);
+            int WinningPosition = 100;
 
-
-            Random random = new Random();
-            int Dice = random.Next(0, 7);
-
-            Console.WriteLine("After rolls the dice ,Player at postion : " + Dice);
-            int option = random.Next(0, 7);
-            if (option == 0)
+            while (position <= WinningPosition)
             {
-                Console.WriteLine("No play ");
+
+                Random random = new Random();
+                int Dice = random.Next(0, 7);
+
+                Console.WriteLine("Rolls the dice : " + Dice);
+
+                if (Dice == 0)
+                {
+                    Console.WriteLine("No play ");
 
 
-            }
-            else if (option == 1)
-            {
-                Console.WriteLine("Ladder");
-                position += Dice;
-                Console.WriteLine("Player at positon " + position);
+                }
+                else if (Dice == 1)
+                {
+                    Console.WriteLine("Ladder");
+                    position += Dice;
+                    Console.WriteLine("Player at positon " + position);
 
-            }
-            else if (option == 3)
-            {
-                Console.WriteLine("Snake");
-                position -= Dice;
+                }
+                else if (Dice == 3)
+                {
+                    Console.WriteLine("Snake");
+                    position -= Dice;
+                    Console.WriteLine("Player at position " + position);
+
+                    if (Dice < 0)
+                    {
+                        position = 0;
+                        Console.WriteLine("Current position is :  " + position);
+                    }
+
+                }
+
+                position = position + Dice;
                 Console.WriteLine("Player at position " + position);
 
+                position++;
+                if (position == WinningPosition)
+                {
+                    Console.WriteLine("Player wins");
+                    break;
+                }
+
             }
+
         }
-    }
+
+    }        
+    
 }    
     
