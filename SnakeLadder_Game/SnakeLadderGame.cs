5ce4d2f7c6ca272f267ae1b528c5789277a20bf7
Shivani21-hude <shivani21hude@gmail.com>
2023-01-07 20:15:35 +0530

    [Shivani]-Report which player won the game betwwen Two players

--- SnakeLadder_Game/SnakeLadderGame.cs
+++ SnakeLadder_Game/SnakeLadderGame.cs
@@ -8,76 +8,126 @@ namespace SnakeLadder_Game
 {
     public class SnakeLadderGame
     {
-        public static void CountNoOfRolls()
-        {
-
-
-            int position = 0;
-            int WinningPosition = 100;
-            int count = 0;
-            while (position <= WinningPosition)
+       public static void PlayingTwoPlayers()
             {
 
-                Random random = new Random();
-                int Dice = random.Next(1, 7);
 
-                Console.WriteLine("Rolls the dice : " + Dice);
+                int WinningPosition = 100;
+                int count = 0;
+                int Player1 = 0, Player2 = 0;
 
-                int option = random.Next(3);
-                switch (option)
+
+                while (Player1 <= WinningPosition && Player2 <= WinningPosition)
                 {
-                    case 0:
-                        Console.WriteLine("No Play");
-                        break;
-                    case 1:
-                        Console.WriteLine("Ladder");
-                        position += Dice;
-                        Console.WriteLine("Current Position is ==>" + position);
 
-                        if (position > 100)
-                        {
-                            position = position - Dice;
-                            Console.WriteLine("Current Position is ==>" + position);
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake");
-                        position -= Dice;
-                        Console.WriteLine("Current Position is ==>" + position);
+                    Console.WriteLine(" Player 1 => \n");
 
-                        if (position < 0)
-                        {
-                            position = 0;
-                            Console.WriteLine("Current Position is ==>" + position);
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Not Correct");
-                        break;
-                }
-                        position = position + Dice;
-                        Console.WriteLine("Player at position " + position);
+                    Random random1 = new Random();
+                    int Dice1 = random1.Next(1, 7);
 
-                        if (position == WinningPosition)
-                        {
-                            Console.WriteLine("<========= Player wins ============>");
+
+                    Console.WriteLine("Player 1 Rolls the dice : " + Dice1);
+                    int option1 = random1.Next(3);
+                    count++;
+                    if (Player1 == WinningPosition)
+                    {
+                        Console.WriteLine("<=============== Player 1 wins ==================>\n");
+                        break;
+
+                    }
+                    switch (option1)
+                    {
+                        case 0:
+                            Console.WriteLine("No Play");
                             break;
-                        }
-                else if (position < 0)
-                {
-                    position = 0;
-                    Console.WriteLine("Current Position is ==>" + position);
+                        case 1:
+                            Console.WriteLine("Ladder");
+                            Player1 += Dice1;
+                            Console.WriteLine("Current Position is ==>" + Player1);
+
+                            if (Player1 > 100)
+                            {
+                                Player1 = Player1 - Dice1;
+                                Console.WriteLine("Current Position is ==>" + Player1);
+                            }
+
+                            break;
+                        case 2:
+                            Console.WriteLine("Snake");
+                            Player1 -= Dice1;
+                            Console.WriteLine("Current Position is ==>" + Player1);
+
+                            if (Player1 < 0)
+                            {
+                                Player1 = 0;
+                                Console.WriteLine("Current Position is ==>" + Player1);
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Not Correct");
+                            break;
+
+                    }
+
+                    Console.WriteLine("..................................................");
+
+                    Console.WriteLine(" Player 2.=> \n");
+
+                    Random random = new Random();
+                    int Dice = random.Next(1, 7);
+
+
+                    Console.WriteLine("Player 2 Rolls the dice : " + Dice);
+                    int option = random.Next(3);
+                    if (Player2 == WinningPosition)
+                    {
+                        Console.WriteLine("<============== Player 2 wins ==============>\n");
+                        break;
+                    }
+                    switch (option)
+                    {
+                        case 0:
+                            Console.WriteLine("No Play");
+                            break;
+                        case 1:
+                            Console.WriteLine("Ladder");
+                            Player2 += Dice;
+                            Console.WriteLine("Current Position is ==>" + Player2);
+
+                            if (Player2 > 100)
+                            {
+                                Player2 = Player2 - Dice;
+                                Console.WriteLine("Current Position is ==>" + Player2);
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Snake");
+                            Player2 -= Dice;
+                            Console.WriteLine("Current Position is ==>" + Player2);
+
+                            if (Player2 < 0)
+                            {
+                                Player2 = 0;
+                                Console.WriteLine("Current Position is ==>" + Player2);
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Not Correct");
+                            break;
+
+                    }
+                    Console.WriteLine("..................................................");
+
                 }
 
+                if (Player1 == WinningPosition)
+                    Console.WriteLine("***Player 1 Won The Game***");
+                else
+                    Console.WriteLine("***Player 2 Won The Game***");
 
 
-                count++;
-
-
-
-            }
-            Console.WriteLine("No of dice rolls" + count);
-
+                Console.WriteLine("Players rolls dices  \n  " + count + "times");
+            
         }
     }
 }
