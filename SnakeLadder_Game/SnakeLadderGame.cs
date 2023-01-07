f7bcc54f343d6cfb2395726ee4036d20c2fefb33
Shivani21-hude <shivani21hude@gmail.com>
2023-01-07 17:38:07 +0530

    [Shivani]-Count the number of times dice rolls

--- SnakeLadder_Game/SnakeLadderGame.cs
+++ SnakeLadder_Game/SnakeLadderGame.cs
@@ -8,66 +8,67 @@ namespace SnakeLadder_Game
 {
     public class SnakeLadderGame
     {
-        public static void U54WinningPosition()
+        public static void UC6Countrolls()
         {
-                int position = 0;
-                int WinningPosition = 100;
 
-                while (position <= WinningPosition)
+            int position = 0;
+            int WinningPosition = 100;
+            int count = 0;
+            while (position <= WinningPosition)
+            {
+
+                Random random = new Random();
+                int Dice = random.Next(0, 7);
+
+
+                Console.WriteLine("Rolls the dice : " + Dice);
+
+                if (Dice == 0)
                 {
+                    Console.WriteLine("No play ");
 
-                    Random random = new Random();
-                    int Dice = random.Next(1, 7);
+                }
+                else if (Dice == 5)
+                {
+                    Console.WriteLine("Ladder");
+                    position += Dice;
+                    Console.WriteLine("Player at positon " + position);
 
-                    Console.WriteLine("Rolls the dice : " + Dice);
-
-                    int option = random.Next(3);
-                    if (option == 0)
-                    {
-                        Console.WriteLine("No play ");
-
-
-                    }
-                    else if (option == 1)
-                    {
-                        Console.WriteLine("Ladder");
-                        position += Dice;
-                        Console.WriteLine("Player at positon " + position);
-
-                    }
-                    else if (option == 2)
-                    {
-                        Console.WriteLine("Snake");
-                        position -= Dice;
-                        Console.WriteLine("Player at position " + position);
-
-                        if (option < 0)
-                        {
-                            position = 0;
-                            Console.WriteLine("Current position is :  " + position);
-                        }
-
-                    }
-
-                    position = position + Dice;
+                }
+                else if (Dice == 3)
+                {
+                    Console.WriteLine("Snake");
+                    position -= Dice;
                     Console.WriteLine("Player at position " + position);
 
-
-                    if (position == WinningPosition)
+                    if (Dice < 0)
                     {
-                        Console.WriteLine("Player wins");
-                        break;
-                    }
-                    else if (position > WinningPosition)
-                    {
-                        Console.WriteLine("Player position is greater than winning position");
-                        position = position - Dice;
-
-
+                        position = 0;
+                        Console.WriteLine("Current position is :  " + position);
                     }
 
                 }
-            
+
+                position = position + Dice;
+                Console.WriteLine("Player at position " + position);
+
+                if (position == WinningPosition)
+                {
+                    Console.WriteLine("Player wins");
+                    break;
+                }
+                else if (position > WinningPosition)
+                {
+                    Console.WriteLine("Player position is greater than winning position");
+                    position = position - Dice;
+
+                }
+                count++;
+
+            }
+
+            Console.WriteLine("No of dice rolls" + count);
+
         }
 
     }
